bc458ba53af87b35fb27b5b491cb6d72d670e0e2
kas <kas1kas@hotmail.com>
2026-02-23 20:31:45 +0100

    Refactor update logic in effect_normal.py
    Removed first_update flag and related logic to simplify update behavior.

--- effects/effect_normal.py
+++ effects/effect_normal.py
@@ -7,26 +7,16 @@ class EffectNormal(BaseEffect):
     def __init__(self, word_clock):
         super().__init__(word_clock)
         self.last_minute = -1
-        self.first_update = True  # Add this flag
     
-    def start(self):
-        """Reset first_update flag when effect starts"""
-        self.first_update = True
+    def reset_timing(self):
+        """Reset so next update shows time immediately"""
         self.last_minute = -1
     
     def update(self):
         current_minute = time.localtime().tm_min
-        
-        # Always show time on first update after switching
-        if self.first_update:
-            self.first_update = False
+        if current_minute != self.last_minute:
             self.last_minute = current_minute
-            self.word_clock.cls()
-            self.word_clock.update_clock()
-        # Otherwise only update when minute changes
-        elif current_minute != self.last_minute:
-            self.last_minute = current_minute
-            self.word_clock.cls()
+            # Don't clear! The main program clears when needed
             self.word_clock.update_clock()
     
     def get_settings_template(self):
