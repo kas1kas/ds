0e4fa27655c3810e02fb37a94af340a3e80a82f8
kas <kas1kas@hotmail.com>
2026-02-23 17:03:50 +0100

    Enhance EffectNormal with minute tracking
    Add initialization and minute change check to EffectNormal.

--- effects/effect_normal.py
+++ effects/effect_normal.py
@@ -4,6 +4,17 @@ class EffectNormal(BaseEffect):
     name = "Normal Clock"
     description = "Standard word clock display"
     
+    def __init__(self, word_clock):
+        super().__init__(word_clock)
+        self.last_minute = -1
+    
     def update(self):
-        self.word_clock.cls()
-        self.word_clock.update_clock()
+        # Check if minute changed
+        current_minute = time.localtime().tm_min
+        if current_minute != self.last_minute:
+            self.last_minute = current_minute
+            self.word_clock.cls()
+            self.word_clock.update_clock()
+    
+    def get_settings_template(self):
+        return "<div>Normal clock mode - no additional settings</div>"
