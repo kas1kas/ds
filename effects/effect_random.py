3499e32379cab01c63e0f68e8db8c4c49add89a2
kas <kas1kas@hotmail.com>
2026-02-24 16:17:15 +0100

    Update effect_random.py

--- effects/effect_random.py
+++ effects/effect_random.py
@@ -1,10 +1,30 @@
+import random
+import time
+from effects.base_effect import BaseEffect
+
 class EffectRandom(BaseEffect):
+    name = "Random"
+    
+    def __init__(self, word_clock):
+        super().__init__(word_clock)
+        self.last_update = 0
+        self.update_interval = 0.02  # 50fps
+    
     def draw(self):
-        # Draw random LEDs ON TOP of existing display
-        for _ in range(20):
+        """Draw random LEDs with time overlay"""
+        current_time = time.time()
+        if current_time - self.last_update < self.update_interval:
+            return
+        
+        self.last_update = current_time
+        
+        # Draw random LEDs (background)
+        num_leds = random.randint(20, 30)
+        for _ in range(num_leds):
             x = random.randint(0, self.word_clock.columns - 1)
             y = random.randint(0, self.word_clock.rows - 1)
             color = self.word_clock.random_color(self.word_clock.rand_color)
             self.word_clock.setcolor_x_y(x, y, color)
         
-        self.word_clock.update_clock()  # Draw time on top
+        # Draw time on top (clears old words automatically)
+        self.word_clock.update_clock()
