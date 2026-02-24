d02ae467fe87b1849be7546c0d95b5e43cfd65a2
kas <kas1kas@hotmail.com>
2026-02-24 18:35:43 +0100

    Update effect_normal.py

--- effects/effect_normal.py
+++ effects/effect_normal.py
@@ -4,18 +4,7 @@ from effects.base_effect import BaseEffect
 class EffectNormal(BaseEffect):
     name = "Normal Clock"
     
-    def __init__(self, word_clock):
-        super().__init__(word_clock)
-        self.last_minute = -1
-        self.first_draw = True  # Simple flag
-    
     def draw(self):
-        """Clear and show time"""
-        current_minute = time.localtime().tm_min
-        
-        # Draw on first frame OR when minute changes
-        if self.first_draw or current_minute != self.last_minute:
-            self.first_draw = False
-            self.last_minute = current_minute
-            self.word_clock.cls()
-            self.word_clock.update_clock()
+        """Clear and show time every frame"""
+        self.word_clock.cls()
+        self.word_clock.update_clock()
