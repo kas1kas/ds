67dcf5f4ba3274edfb5394a716a1305b9c8b1591
kas <kas1kas@hotmail.com>
2026-02-24 18:28:24 +0100

    Update effect_oeteldonk.py

--- effects/effect_oeteldonk.py
+++ effects/effect_oeteldonk.py
@@ -3,32 +3,31 @@ from effects.base_effect import BaseEffect
 
 class EffectOeteldonk(BaseEffect):
     name = "Oeteldonk"
-    description = "Oeteldonk theme"
     
     def __init__(self, word_clock):
         super().__init__(word_clock)
-        self.saved_color = None
         self.last_update = 0
     
-    def draw(self):  # Change from update() to draw()
+    def draw(self):
         current_time = time.time()
         if current_time - self.last_update < 60:  # Update every minute
             return
         
         self.last_update = current_time
-        self.oeteldonk_background()
-        # Set time color to cyan
+        
+        # Draw Oeteldonk background colors
+        for x in range(self.word_clock.columns):
+            for y in range(self.word_clock.rows):
+                if y > 6:
+                    color = (169, 169, 0)    # Yellow top
+                elif y > 2:
+                    color = (169, 169, 169)  # Grey middle
+                else:
+                    color = (169, 0, 0)      # Red bottom
+                self.word_clock.setcolor_x_y(x, y, color)
+        
+        # Save original color, set time to cyan, then restore
+        original_color = self.word_clock.letter_active_color
         self.word_clock.letter_active_color = (0, 255, 255)
         self.word_clock.update_clock()
-        # Restore original color for next time? Actually no, keep cyan for time
-    
-    def oeteldonk_background(self):
-        for x in range(11):
-            for y in range(10):
-                if y > 6:
-                    bgcolor = [169, 169, 0]
-                elif y > 2:
-                    bgcolor = [169, 169, 169]
-                else:
-                    bgcolor = [169, 0, 0]
-                self.word_clock.setcolor_x_y(x, y, bgcolor)
+        self.word_clock.letter_active_color = original_color
