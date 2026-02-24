70c1268ad4060878de2ee29a8d1c17055d891400
kas <kas1kas@hotmail.com>
2026-02-24 18:34:34 +0100

    Update effect_oeteldonk.py

--- effects/effect_oeteldonk.py
+++ effects/effect_oeteldonk.py
@@ -4,18 +4,8 @@ from effects.base_effect import BaseEffect
 class EffectOeteldonk(BaseEffect):
     name = "Oeteldonk"
     
-    def __init__(self, word_clock):
-        super().__init__(word_clock)
-        self.last_update = 0
-    
     def draw(self):
-        current_time = time.time()
-        if current_time - self.last_update < 60:  # Update every minute
-            return
-        
-        self.last_update = current_time
-        
-        # Draw Oeteldonk background colors
+        # Draw Oeteldonk background colors every frame
         for x in range(self.word_clock.columns):
             for y in range(self.word_clock.rows):
                 if y > 6:
@@ -26,7 +16,7 @@ class EffectOeteldonk(BaseEffect):
                     color = (169, 0, 0)      # Red bottom
                 self.word_clock.setcolor_x_y(x, y, color)
         
-        # Save original color, set time to cyan, then restore
+        # Set time to cyan
         original_color = self.word_clock.letter_active_color
         self.word_clock.letter_active_color = (0, 255, 255)
         self.word_clock.update_clock()
