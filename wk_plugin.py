f0528a5b36520456f0e0709f2c2227e631742856
kas <kas1kas@hotmail.com>
2026-02-24 20:21:29 +0100

    Update wk_plugin.py

--- wk_plugin.py
+++ wk_plugin.py
@@ -490,9 +490,6 @@ def load_merged_config():
 config = load_merged_config()
 word_clock = WordClock(config)
 
-# Import web routes (must be after word_clock is created)
-import web_routes
-
 # Main function to run the word clock
 # In run_clock function, add frame counter:
 
@@ -522,6 +519,9 @@ def run_clock():
     finally:
         word_clock.cls()
         word_clock.strip.show()
+
+# Import web routes (must be after word_clock is created)
+import web_routes
 
 if __name__ == "__main__":
     # Start the Flask web server in a separate thread
