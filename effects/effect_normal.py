faf450235803268cb2eb81fdd9d4e4063721107c
kas <kas1kas@hotmail.com>
2026-02-24 16:18:03 +0100

    Update effect_normal.py

--- effects/effect_normal.py
+++ effects/effect_normal.py
@@ -1,4 +1,17 @@
+import time
+from effects.base_effect import BaseEffect
+
 class EffectNormal(BaseEffect):
+    name = "Normal Clock"
+    
+    def __init__(self, word_clock):
+        super().__init__(word_clock)
+        self.last_minute = -1
+    
     def draw(self):
-        self.word_clock.cls()           # Clear first
-        self.word_clock.update_clock()  # Then draw time
+        """Clear and show time - updates every minute"""
+        current_minute = time.localtime().tm_min
+        if current_minute != self.last_minute:
+            self.last_minute = current_minute
+            self.word_clock.cls()               # Clear everything
+            self.word_clock.update_clock()       # Draw time
