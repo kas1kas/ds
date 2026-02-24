c8d28990766dd95ff67f3336421024dccdd50612
kas <kas1kas@hotmail.com>
2026-02-24 08:25:50 +0100

    Update effect_random.py

--- effects/effect_random.py
+++ effects/effect_random.py
@@ -1,65 +1,38 @@
-import time
 import random
+import time
 from .base_effect import BaseEffect
 
 class EffectRandom(BaseEffect):
     name = "Random"
-    description = "Fast random colors with tint"
+    description = "Randomly colored LEDs"
     
     def __init__(self, word_clock):
         super().__init__(word_clock)
         self.last_update = 0
-        self.update_interval = 0.001  # Fast - 1ms
-        self.tint = word_clock.rand_color  # Get tint from config
+        self.update_interval = 0.05  # 50ms for smooth animation
     
-    def random_color(self):
-        """Random color based on tint - moved from WordClock class"""
-        if self.tint == "blue":
-            r = random.randint(29, 69)    # shades of blue 
-            g = random.randint(31, 71)
-            b = random.randint(105, 245)
-        elif self.tint == "orange":
-            r = random.randint(100, 155)  # shades of orange 
-            g = random.randint(20, 40)
-            b = random.randint(0, 2)
-        else:
-            # Default to blue if tint is invalid
-            r = random.randint(29, 69)
-            g = random.randint(31, 71)
-            b = random.randint(105, 245)
-        return (r, g, b)
-    
-    def start(self):
-        """Called when effect starts"""
-        self.logger.info(f"Starting random effect with {self.tint} tint")
-        # Clear screen first
-        self.word_clock.cls()
-    
-    def update(self):
+    def draw(self):
         current_time = time.time()
         if current_time - self.last_update < self.update_interval:
             return
         
         self.last_update = current_time
         
-        # Get random position
-        x = random.randint(0, self.word_clock.columns - 1)
-        y = random.randint(0, self.word_clock.rows - 1)
+        # Clear and set random LEDs
+        self.word_clock.cls()
         
-        # Get random color using local method
-        color = self.random_color()
+        # Set 10-20 random LEDs per frame
+        num_leds = random.randint(10, 20)
+        for _ in range(num_leds):
+            x = random.randint(0, self.word_clock.columns - 1)
+            y = random.randint(0, self.word_clock.rows - 1)
+            r = random.randint(0, 255)
+            g = random.randint(0, 255)
+            b = random.randint(0, 255)
+            self.word_clock.setcolor_x_y(x, y, (r, g, b))
         
-        # Set the LED
-        self.word_clock.setcolor_x_y(x, y, color)
-        
-        # Update clock display
+        # Show time overlay
         self.word_clock.update_clock()
     
     def get_settings_template(self):
-        """Show current tint setting"""
-        return f"""
-        <div class="random-settings">
-            <p>Random effect - <b>{self.tint}</b> tint</p>
-            <p>Speed: Fast (original)</p>
-        </div>
-        """
+        return "<div>Random mode - no additional settings</div>"
