18cfb57438d36249dcada21f537203b06caf9ed5
kas <kas1kas@hotmail.com>
2026-02-24 09:52:10 +0100

    Update effect_normal.py
    super simple

--- effects/effect_normal.py
+++ effects/effect_normal.py
@@ -1,20 +1,15 @@
 import time
-from effects.base_effect import BaseEffect  # Absolute import
+from effects.base_effect import BaseEffect
 
 class EffectNormal(BaseEffect):
     name = "Normal Clock"
-    description = "Standard word clock display"
     
     def __init__(self, word_clock):
         super().__init__(word_clock)
         self.last_minute = -1
     
     def draw(self):
-        """Update when minute changes"""
         current_minute = time.localtime().tm_min
         if current_minute != self.last_minute:
             self.last_minute = current_minute
-            self.word_clock.update_clock()  # This clears and shows time
-    
-    def get_settings_template(self):
-        return "<div>Normal clock mode - no additional settings</div>"
+            self.word_clock.update_clock()
