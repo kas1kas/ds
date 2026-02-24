845b54a12d29e671ef5ca9b597ba7a3e069a59f1
kas <kas1kas@hotmail.com>
2026-02-24 09:57:44 +0100

    Update effect_normal.py

--- effects/effect_normal.py
+++ effects/effect_normal.py
@@ -4,12 +4,6 @@ from effects.base_effect import BaseEffect
 class EffectNormal(BaseEffect):
     name = "Normal Clock"
     
-    def __init__(self, word_clock):
-        super().__init__(word_clock)
-        self.last_minute = -1
-    
     def draw(self):
-        current_minute = time.localtime().tm_min
-        if current_minute != self.last_minute:
-            self.last_minute = current_minute
-            self.word_clock.update_clock()
+        """Draw the clock every frame"""
+        self.word_clock.update_clock()
