82df9c4a5eb5f89d6fea4b0e24a335e9af486aac
kas <kas1kas@hotmail.com>
2026-02-25 15:28:15 +0100

    Update effect_random.py

--- effects/effect_random.py
+++ effects/effect_random.py
@@ -5,18 +5,19 @@ from effects.base_effect import BaseEffect
 class EffectRandom(BaseEffect):
     name = "Random"
     
-    def __init__(self, word_clock):
-        super().__init__(word_clock)
+    def __init__(self, word_clock, variant_id=None):
+        super().__init__(word_clock, variant_id)
         self.last_update = 0
-        self.update_interval = 0.02  # 50fps
+        self.update_interval = 0.02  # 50 fps – smooth twinkling
     
     def draw(self):
         current_time = time.time()
         if current_time - self.last_update < self.update_interval:
             return
-        
         self.last_update = current_time
         
-        # Just like the original: set one random LED, then show time
+        # Set one random LED with the configured color tint
         self.word_clock.set_random_led(self.word_clock.rand_color)
+        
+        # Overlay the time (this also calls strip.show())
         self.word_clock.update_clock()
