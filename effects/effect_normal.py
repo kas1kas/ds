bf774b56e9bd1df9b7ba6faf813c61fd4ba73ac7
kas <kas1kas@hotmail.com>
2026-02-25 15:07:10 +0100

    Update effect_normal.py

--- effects/effect_normal.py
+++ effects/effect_normal.py
@@ -5,6 +5,5 @@ class EffectNormal(BaseEffect):
     name = "Normal Clock"
     
     def draw(self):
-        """Clear and show time every frame"""
         self.word_clock.cls()
         self.word_clock.update_clock()
