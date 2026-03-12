633fc2b3df4a86114c20cde13b096db9a33321ac
kas <kas1kas@hotmail.com>
2026-03-12 09:13:11 +0100

    normal

--- effects/effect_normal.py
+++ effects/effect_normal.py
@@ -2,7 +2,7 @@ import time
 from effects.base_effect import BaseEffect
 
 class EffectNormal(BaseEffect):
-    name = "Normal Clock"
+    name = "Normal"
     
     def draw(self):
         self.word_clock.cls()
