5b94d12d6f4e1a050b99bc99ef58f363de4bf790
kas <kas1kas@hotmail.com>
2026-03-13 15:14:07 +0100

    random colors in effect

--- effects/effect_random.py
+++ effects/effect_random.py
@@ -13,6 +13,34 @@ class EffectRandom(BaseEffect):
         # Store the tint for later use
         self.tint = word_clock.rand_color
     
+    def _generate_random_color(self):
+        """Generate random color based on tint"""
+        if self.tint == "blue":
+            r = random.randint(29, 69)
+            g = random.randint(31, 71)
+            b = random.randint(105, 245)
+        elif self.tint == "orange":
+            r = random.randint(100, 155)
+            g = random.randint(20, 40)
+            b = random.randint(0, 2)
+        elif self.tint == "red":
+            r = random.randint(200, 255)
+            g = random.randint(0, 50)
+            b = random.randint(0, 50)
+        elif self.tint == "green":
+            r = random.randint(0, 50)
+            g = random.randint(200, 255)
+            b = random.randint(0, 50)
+        elif self.tint == "purple":
+            r = random.randint(150, 255)
+            g = random.randint(0, 50)
+            b = random.randint(150, 255)
+        else:  # full random
+            r = random.randint(0, 255)
+            g = random.randint(0, 255)
+            b = random.randint(0, 255)
+        return (r, g, b)
+    
     def draw(self):
         current_time = time.time()
         if current_time - self.last_update < self.update_interval:
@@ -25,7 +53,7 @@ class EffectRandom(BaseEffect):
         # Set one random LED
         x = random.randint(0, max_cols - 1)
         y = random.randint(0, max_rows - 1)
-        color = self.word_clock.random_color(self.tint)
+        color = self._generate_random_color()
 
         # Apply current background brightness dynamically
         # This will use the latest slider value
